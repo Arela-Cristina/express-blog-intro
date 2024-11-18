95511604518d189cf070db145335b8523f7db28d
Arela-Cristina <instagrandine@gmail.com>
2024-11-18 16:26:24 +0100

    fix error: change ES Modules syntax instead of  Common js, then import on main.js,  arrays.js doc

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 console.log("Hello World");
+
 // STEP 1
-//Creiamo un server basico
-const express = require("express"); //importare modulo express in main.js
+//Creiamo un server basico ES Modules
+import express from "express"; //importare modulo express in main.js
 // console.log(express); //oggetto
 
 const app = express(); //inizializzamo express. salviamo la istanzia(il server: App), per uttilizzare dei metodi dopo
@@ -9,6 +10,9 @@ const app = express(); //inizializzamo express. salviamo la istanzia(il server: 
 
 const port = 666; //assegniamo un numero alla porta principale della nostra app
 console.log(port);
+
+//import from arrays.js
+import { posts } from "./arrays.js";
 
 //STEP 4
 //diciamo a express di gestire la risposta delle immagini statiche salvate nel nostro server
