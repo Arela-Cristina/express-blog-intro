58e20b46d7838d0e1b214cfe7b75a0595bcd7d0f
Arela-Cristina <instagrandine@gmail.com>
2024-11-18 14:45:32 +0100

    create a basic server

--- main.js
+++ main.js
@@ -4,4 +4,12 @@ console.log("Hello World");
 
 //importare modulo express in main.js
 const express = require("express");
-console.log(express); //oggetto
+// console.log(express); //oggetto
+
+//inizializzamo express. salviamo la istanzia, per uttilizzare dei metodi dopo
+const app = express();
+// console.log(app); //oggetto
+
+//assegniamo un numero alla porta principale della nostra app
+const port = 666;
+console.log(port);
