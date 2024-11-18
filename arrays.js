e650bf29a8499cb9af60cec77adfc7900b5b9539
Arela-Cristina <instagrandine@gmail.com>
2024-11-18 15:50:05 +0100

    create  posts array

---
+++ arrays.js
@@ -0,0 +1,22 @@
+const posts = [
+  {
+    title: "Post 1",
+    img: "/images/ciambellone.jpeg",
+  },
+  {
+    title: "Post 2",
+    img: "/images/cracker_barbabietola.jpeg",
+  },
+  {
+    title: "Post 3",
+    img: "/images/pane_fritto_dolce.jpeg",
+  },
+  {
+    title: "Post 4",
+    img: "/images/pasta_barbabietola.jpeg",
+  },
+  {
+    title: "Post 5",
+    img: "/images/torta_paesana.jpeg",
+  },
+];
