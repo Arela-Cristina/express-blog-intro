bb9cde8de8ccd610370db1effeecec33bcaccd55
Arela-Cristina <instagrandine@gmail.com>
2024-11-18 14:17:25 +0100

    import express module

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log("Hello World");
+
+//Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto “Server del mio blog”
+
+//importare modulo express in main.js
+const express = require("express");
+console.log(express); //oggetto
