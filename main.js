40ba60bb343b52dc0a65484d6fc2107e5bbc3e74
Arela-Cristina <instagrandine@gmail.com>
2024-11-18 15:21:50 +0100

    communication between server and client

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ console.log("Hello World");
 const express = require("express"); //importare modulo express in main.js
 // console.log(express); //oggetto
 
-const app = express(); //inizializzamo express. salviamo la istanzia, per uttilizzare dei metodi dopo
+const app = express(); //inizializzamo express. salviamo la istanzia(il server: App), per uttilizzare dei metodi dopo
 // console.log(app); //oggetto
 
 const port = 666; //assegniamo un numero alla porta principale della nostra app
@@ -16,4 +16,9 @@ app.get("/", (req, res) => {
   //callback req, res
   console.log("Server del mio blog");
   res.send("Benvenuto al server del inferno"); //proprieta .send per inviare una  risposta al client
+});
+
+//facciamo ascoltare il server dal client .listen
+app.listen(port, () => {
+  console.log(`Il server del inferno ascolta http://localhost:${port}`);
 });
